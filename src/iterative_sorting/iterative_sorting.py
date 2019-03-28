0301ba878820feff45ca2ccc01a1484a3c39881f
darenlarson <larson.daren@gmail.com>
2019-03-28 16:46:57 -0500

    count_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -37,6 +37,54 @@ def bubble_sort( arr ):
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
 
-    return arr
+def count_sort( arr, maximum=-1 ):
+    print(arr)
+
+    if len(arr) == 0:
+        return []
+
+    if min(arr) < 0:
+        return "Error, negative numbers not allowed in Count Sort"
+    
+    # The output character array that will have sorted arr 
+    output = [0 for i in range(20)] 
+    print("output:", output)
+  
+    # Create a count array to store count of inidividul 
+    # characters and initialize count array as 0 
+    count = [0 for i in range(20)] 
+    print("count:", count)
+  
+    # For storing the resulting answer since the  
+    # string is immutable 
+    ans = [0 for i in arr]
+    print("ans:", ans)
+  
+    # Store count of each character 
+    for i in arr: 
+        count[i] += 1
+        print("i, count[ord(i)]:", i, count[i])
+  
+    # Change count[i] so that count[i] now contains actual 
+    # position of this character in output array 
+    for i in range(20): 
+        count[i] += count[i-1]
+    print("updated count:", count)
+  
+    # Build the output character array 
+    for i in range(len(arr)): 
+        output[count[arr[i]]-1] = arr[i] 
+        count[arr[i]] -= 1
+    print("Updated output:", output)
+  
+    # Copy the output array to arr, so that arr now 
+    # contains sorted characters 
+    for i in range(len(arr)): 
+        ans[i] = output[i]
+    print(ans)
+
+    return ans
+
+# arr = [-1,4,10,8,2,4]
+# print(count_sort(arr))
