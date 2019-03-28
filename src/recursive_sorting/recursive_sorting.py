843fc6a81ed98a996bb273c43ff58e92bcb8bd88
darenlarson <larson.daren@gmail.com>
2019-03-28 17:23:24 -0500

    Fixed merged helper function to work when comparing the same number

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -7,14 +7,15 @@ def merge(arrA, arrB):
     arrAIndex = 0
     arrBIndex = 0
 
-    # print("arrA: ", arrA)
-    # print("arrB: ", arrB)
-    # print("elements: ", elements)
-    # print("merged_arr: ", merged_arr)
+    print("arrA: ", arrA)
+    print("arrB: ", arrB)
+    print("elements: ", elements)
+    print("merged_arr: ", merged_arr)
 
 
     # for all elements:
     for i in range(0, elements):
+        print(f"---------------FOR LOOP START--------------- {i}")
         print("arrA:", arrA)
         print("arrB:", arrB)
         print("elements:", elements)
@@ -25,6 +26,7 @@ def merge(arrA, arrB):
         # if all elements in arrA have been merged, put next element in arrB into merged array
         # if len(arrA) = 0:
         if arrAIndex > len(arrA)-1 and arrBIndex <= len(arrB)-1:
+            print("IF STATEMENT 1 TRIGGERED")
             merged_arr[i] = arrB[arrBIndex]
             # arrB.pop([0])
             arrBIndex += 1
@@ -32,22 +34,26 @@ def merge(arrA, arrB):
         # elif all elements in arrB have been merged, put next element in arrA into merged array
         # if len(arrB) = 0:
         elif arrBIndex > len(arrB)-1 and arrAIndex <= len(arrA)-1:
+            print("IF STATEMENT 2 TRIGGERED")
             merged_arr[i] = arrA[arrAIndex]
             # arrB.pop([0])
             arrAIndex += 1
 
-        # elif next element in arrA smaller, add to merged array
-        elif arrA[arrAIndex] < arrB[arrBIndex]:
+        # elif next element in arrA smaller or equal, add to merged array
+        elif arrA[arrAIndex] <= arrB[arrBIndex]:
+            print("IF STATEMENT 3 TRIGGERED")
             merged_arr[i] = arrA[arrAIndex]
             # arrA.pop([0])
             arrAIndex += 1
 
         # else next element in arrB smaller, add to merged array
         elif arrB[arrBIndex] < arrA[arrAIndex]:
+            print("IF STATEMENT 4 TRIGGERED")
             merged_arr[i] = arrB[arrBIndex]
             # arrB.pop([0])
             arrBIndex += 1
     
+    print("RETURNED merged_arr:", merged_arr)
     return merged_arr
 
 
@@ -60,6 +66,12 @@ def merge_sort( arr ):
         arr = merge(left, right)
 
     return arr
+
+testArray = [6,23,5,1,6,7,9,7,3,1,35,1,5,5,6,434,341,41]
+# testArray = [3,2,1,1]
+# testArray = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(merge_sort(testArray))
+
 
 # TO HELP MENTALLY THINK ABOUT HOW THE RECURSION FLOWS
 # merge_sort([1,3,2,4])
